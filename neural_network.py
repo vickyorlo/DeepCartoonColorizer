a4c5aebac05277c058956bcb7f2b64fe3e68d10c
Mariusz Chybicki <maniusz28@wp.pl>
2018-04-12 21:29:59 +0200

    network structure update

--- neural_network.py
+++ neural_network.py
@@ -1,4 +1,5 @@
-from keras.layers import Conv2D, UpSampling2D, Input, Reshape, concatenate, MaxPooling2D, Dropout
+from keras.layers import Conv2D, UpSampling2D, Input, Reshape, concatenate, MaxPooling2D, Dropout, BatchNormalization, GaussianDropout, AlphaDropout
+from keras.layers.advanced_activations import PReLU
 from keras.models import Model
 from keras.preprocessing.image import img_to_array, load_img
 from keras.optimizers import Adamax
@@ -13,14 +14,19 @@ import tensorflow as tf
 
 X = []
 
-for filename in [filename for filename in os.listdir('frames_from_movies/')[110:185] if filename.endswith(".png")]:
+for filename in [filename for filename in os.listdir('frames_from_movies/')[100:120] if filename.endswith(".png")]:
     X.append(img_to_array(load_img('frames_from_movies/' + filename)))
 
 
 X = np.array(X, dtype=float)
 Xtrain = (1.0 / 255) * X
+for i in range(len(Xtrain)):
+    imsave("result/learned_on_what_2/img_" + str(i) + ".png", Xtrain[i])
+
 
 network_input = Input(shape=(256, 256, 1,))
+
+
 
 first_path = Conv2D(4, (3,3), padding='same', activation='relu')(network_input)
 first_path = Conv2D(8, (3,3), padding='same', activation='relu')(first_path)
@@ -34,63 +40,72 @@ inception_output = concatenate([first_path, second_path, third_path], axis = 3)
 network = Conv2D(16, (3, 3), activation='relu', padding='same')(inception_output)
 network = Conv2D(16, (3, 3), activation='tanh', padding='same')(network)
 network = Conv2D(16, (3, 3), activation='relu', padding='same')(network)
+#network = BatchNormalization()(network)
 network = MaxPooling2D((2,2))(network)
-network = Dropout(0.25)(network)
+network = GaussianDropout(0.2)(network)
 network = Conv2D(32, (3, 3), activation='tanh', padding='same')(network)
 network = Conv2D(32, (3, 3), activation='relu', padding='same')(network)
 network = Conv2D(32, (3, 3), activation='tanh', padding='same')(network)
+#network = BatchNormalization()(network)
 network = MaxPooling2D((2,2))(network)
-network = Dropout(0.25)(network)
+network = GaussianDropout(0.2)(network)
 network = Conv2D(64, (3, 3), activation='relu', padding='same')(network)
 network = Conv2D(64, (3, 3), activation='tanh', padding='same')(network)
 network = Conv2D(64, (3, 3), activation='relu', padding='same')(network)
+#network = BatchNormalization()(network)
 network = MaxPooling2D((2,2))(network)
-network = Dropout(0.4)(network)
+network = GaussianDropout(0.2)(network)
 network = Conv2D(128, (3, 3), activation='tanh', padding='same')(network)
 network = Conv2D(128, (3, 3), activation='relu', padding='same')(network)
 network = Conv2D(128, (3, 3), activation='tanh', padding='same')(network)
+#network = BatchNormalization()(network)
 network = MaxPooling2D((2,2))(network)
-network = Dropout(0.4)(network)
+network = GaussianDropout(0.2)(network)
 network = Conv2D(256, (3, 3), activation='relu', padding='same')(network)
 network = Conv2D(256, (3, 3), activation='tanh', padding='same')(network)
 network = Conv2D(256, (3, 3), activation='relu', padding='same')(network)
+#network = BatchNormalization()(network)
 network = UpSampling2D((2, 2))(network)
-network = Dropout(0.4)(network)
+network = GaussianDropout(0.2)(network)
 network = Conv2D(128, (3, 3), activation='tanh', padding='same')(network)
 network = Conv2D(128, (3, 3), activation='relu', padding='same')(network)
 network = Conv2D(128, (3, 3), activation='tanh', padding='same')(network)
+#network = BatchNormalization()(network)
 network = UpSampling2D((2, 2))(network)
-network = Dropout(0.4)(network)
+network = GaussianDropout(0.2)(network)
 network = Conv2D(64, (3, 3), activation='relu', padding='same')(network)
 network = Conv2D(64, (3, 3), activation='tanh', padding='same')(network)
 network = Conv2D(64, (3, 3), activation='relu', padding='same')(network)
+#network = BatchNormalization()(network)
 network = UpSampling2D((2, 2))(network)
-network = Dropout(0.25)(network)
+network = GaussianDropout(0.2)(network)
 network = Conv2D(32, (3, 3), activation='tanh', padding='same')(network)
 network = Conv2D(32, (3, 3), activation='relu', padding='same')(network)
 network = Conv2D(32, (3, 3), activation='tanh', padding='same')(network)
+#network = BatchNormalization()(network)
 network = UpSampling2D((2, 2))(network)
-network = Dropout(0.25)(network)
+network = GaussianDropout(0.2)(network)
 network = Conv2D(16, (3, 3), activation='relu', padding='same')(network)
 network = Conv2D(16, (3, 3), activation='tanh', padding='same')(network)
 network = Conv2D(16, (3, 3), activation='relu', padding='same')(network)
+#network = BatchNormalization()(network)
 
-first_path_2 = Conv2D(8, (3,3), padding='same', activation='relu')(network)
-first_path_2 = Conv2D(4, (3,3), padding='same', activation='relu')(first_path_2)
-second_path_2 = Conv2D(4, (3,3), padding='same', activation='relu')(network)
-second_path_2 = Conv2D(4, (3,3), padding='same', activation='relu')(second_path_2)
-third_path_2 = Conv2D(4, (3,3), padding='same', activation='relu')(network)
-third_path_2 = Conv2D(2, (3,3), padding='same', activation='relu')(third_path_2)
+first_path_2 = Conv2D(16, (3,3), padding='same', activation='relu')(network)
+first_path_2 = Conv2D(8, (3,3), padding='same', activation='relu')(first_path_2)
+second_path_2 = Conv2D(8, (3,3), padding='same', activation='relu')(network)
+second_path_2 = Conv2D(8, (3,3), padding='same', activation='relu')(second_path_2)
+third_path_2 = Conv2D(8, (3,3), padding='same', activation='relu')(network)
+third_path_2 = Conv2D(4, (3,3), padding='same', activation='relu')(third_path_2)
 
 inception_output_2 = concatenate([first_path_2, second_path_2, third_path_2], axis = 3)
 
-network = Conv2D(2, (3, 3), activation='tanh', padding='same')(inception_output_2)
-network_output = Conv2D(2, (3, 3), activation='relu', padding='same')(network)
+network = Conv2D(4, (3, 3), activation='relu', padding='same')(inception_output_2)
+network_output = Conv2D(2, (3, 3), activation='tanh', padding='same')(network)
 
 model = Model(inputs=network_input, outputs=network_output)
 
 batch_size = 1
-EPOCHS = 20
+EPOCHS = 300
 
 def image_a_b_gen(batch_size):
     while True:
@@ -103,15 +118,16 @@ def image_a_b_gen(batch_size):
             yield (X_batch, Y_batch)
 
 opt = Adamax(lr=0.001)
+# tensorboard --logdir=path/to/log-directory
 tbCallback = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=32, write_graph=True, write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None)
-model.compile(optimizer=opt, loss='mse', metrics=['mse', 'acc'])
+model.compile(optimizer=opt, loss='mse', metrics=['mae', 'acc'])
 model.fit_generator(image_a_b_gen(batch_size), epochs=EPOCHS, steps_per_epoch=len(Xtrain), callbacks=[tbCallback])
 
 model.save_weights('model_{}e_1000pic.h5'.format(EPOCHS))
 model.save('model_{}e_1000pic_m.h5'.format(EPOCHS))
 
 color_me = []
-for filename in [filename for filename in os.listdir('test/')[11:21] if filename.endswith(".png")]:
+for filename in [filename for filename in os.listdir('test/')[0:25] if filename.endswith(".png")]:
     color_me.append(img_to_array(load_img('test/' + filename)))
 color_me = np.array(color_me, dtype=float)
 color_me = rgb2lab((1.0 / 255) * color_me)[:, :, :, 0] / 128
