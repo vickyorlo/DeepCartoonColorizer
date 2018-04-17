a3ab4f8a172133c3981aaba5bfa5b63900491b4b
Vic <vickyorlo@gmail.com>
2018-04-17 21:29:59 +0200

    i honestly can't say i understand

--- main.py
+++ main.py
@@ -7,11 +7,8 @@ from neural_network import NeuralNetwork
 from color_directory import PictureColorization
 from movie_preparation import MoviePreparation
 
-if __name__ == "__main__":
-    main()
 
-
-def main(self):
+def main():
     parser = argparse.ArgumentParser(description='parser')
     parser.add_argument('-i', help='folder with movie files')
     parser.add_argument('-m', help='the model file')
@@ -24,14 +21,14 @@ def main(self):
 
     if model is not None and os.path.isfile(model):
         nn = NeuralNetwork('frames_from_movies', epochs, model)
-        if input is not None and os.path.isdir(movies):
+        if movies is not None and os.path.isdir(movies):
             pic_prepare = PicturePreparation(movies)
             pic_prepare.save_images()
             nn.run()
 
         colorizer = PictureColorization(nn.model, "test")
         colorizer.save()
-    elif input is not None and os.path.isdir(movies):
+    elif movies is not None and os.path.isdir(movies):
         pic_prepare = PicturePreparation(movies)
         pic_prepare.save_images()
         nn = NeuralNetwork('frames_from_movies', epochs)
@@ -39,3 +36,7 @@ def main(self):
 
     # movie_prep = MoviePreparation('bw_frames')
     # movie_prep.save_movie()
+
+
+if __name__ == "__main__":
+    main()
