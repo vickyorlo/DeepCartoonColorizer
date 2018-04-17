2341035c605448b82bce32930616f9f7590c5f7c
turbodivx <dawid.sielski@outlook.com>
2018-04-17 16:54:33 +0200

    Message added

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from neural_network import NeuralNetwork
 from color_directory import PictureColorization
 from movie_preparation import MoviePreparation
 
-filename = sys.argv[1]
+try:
+    filename = sys.argv[1]
+except IndexError:
+    print('Please input filename as first argument.')
+    sys.exit(0)
 
 if not os.path.isfile(filename):
     print("No file {} in the directory".format(filename))
