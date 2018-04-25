172567efe242401d348f762af78c36d64ce8bd03
kis <vickyorlo@gmail.com>
2018-04-25 12:03:47 +0200

    Added color only, simplifying model

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def main():
     parser = argparse.ArgumentParser(description='parser')
     parser.add_argument('-i', help='folder with movie files')
     parser.add_argument('-m', help='the model file')
+    parser.add_argument('-c', help='color only')
     parser.add_argument('-e', type=int, help='amount of epochs', default=10)
     parser.add_argument('-b', type=int, help='batch_size', default=1)
     parser.add_argument('-s', type=int, help='how many files to skip when creating learning set', default=25)
@@ -23,13 +24,18 @@ def main():
     batch_size = args.b
     stride = args.s
 
+    if args.c and args.m:
+        nn = NeuralNetwork('frames_from_movies', epochs, batch_size, model)
+        colorizer = PictureColorization(nn.model, "test/")
+        colorizer.save()
+
     if model and os.path.isfile(model):
         nn = NeuralNetwork('frames_from_movies', epochs, batch_size, model)
         if movies is not None and os.path.isdir(movies):
             PicturePreparation().prepare_images(movies,stride)
             nn.run()
 
-        colorizer = PictureColorization(nn.model, "test/test")
+        colorizer = PictureColorization(nn.model, "test/")
         colorizer.save()
     
     elif movies and os.path.isdir(movies):
