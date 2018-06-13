17fadbeed3fcdad03c30feaeabdb3f235e37dba5
turbodivx <dawid.sielski@outlook.com>
2018-06-13 23:38:03 +0200

    Evaluate colorization update

--- evaulate_colorization.py
+++ evaulate_colorization.py
@@ -2,20 +2,18 @@ from imagehash import phash
 from scipy.spatial.distance import hamming
 from PIL import Image
 
+from merge_images.merge_images import zip_images
+
 
 def evaluate_colorization(img1, img2):
     img1_hash = phash(img1)
     img2_hash = phash(img2)
 
-    print(img1_hash, img2_hash)
-
     return hamming(img1_hash, img2_hash)
 
 
 if __name__ == '__main__':
-    img1 = Image.open(r'C:\Users\Dawid\Desktop\ComputerScientistColoringBook\merge_images\bajka1\colored\500.png')
-    img2 = Image.open(r'C:\Users\Dawid\Desktop\ComputerScientistColoringBook\merge_images\bajka1\bw\600.png')
+    zipped = zip_images('merge_images\\bajka1')
 
-    value = evaluate_colorization(img1, img2)
-
-    print(value)
+    for x, y in zipped:
+        print(evaluate_colorization(Image.open(x), Image.open(y)))
